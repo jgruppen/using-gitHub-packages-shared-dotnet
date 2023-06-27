cd41a0fd6698b0cc0de3b24e2ee0ce70b7a3696b
Jarno Gruppwn <jgruppen@hotmail.com>
2023-06-27 14:52:21 +0200

    inital setup

---
+++ Rubicon.Demo.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Rubicon.Demo.Shared.Service service = new Rubicon.Demo.Shared.Service();
+
+int x = 10;
+int y = 20;
+
+int plusResult = service.Plus(x, y);
+int minusResult = service.Minus(x, y);
+
+Console.WriteLine($"Plus:  {x} + {y} = {plusResult}");
+Console.WriteLine($"Minus: {x} + {y} = {minusResult}");
---
+++ Rubicon.Demo.Shared/Service.cs
@@ -0,0 +1,13 @@
+﻿namespace Rubicon.Demo.Shared;
+public class Service
+{
+    public int Plus(int x, int y) 
+    {
+        return x + y;
+    }
+
+    public int Minus(int x, int y) 
+    {
+        return x - y;
+    }
+}
